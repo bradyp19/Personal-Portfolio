d50b1d067e8968943b8a4540966cbd57380f2f32
Park <bpark@microstrategy.com>
2025-06-18 12:48:22 -0400

    UI/ux

--- next.config.js
+++ next.config.js
@@ -3,13 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840, 4096],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 768, 1024],
+    formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   // Allow serving static files from brady-portfolio/public
   assetPrefix: '/brady-portfolio/public',
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
